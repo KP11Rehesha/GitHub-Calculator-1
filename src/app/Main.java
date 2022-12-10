51ad50d484e54b600c33c1d610ca1bc08cc9dfdf
KP11Rehesha <regeshao@gmail.com>
2022-12-11 01:58:33 +0200

    Change app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("My app for users");
 
         myNumbers numbers = declaration();
 
